66462286e757956b0e6c03c7b728464ec4cc7c8a
Toda23 <s-toda7274@st.kyoto-art.ac.jp>
2022-11-04 16:19:48 +0900

    114.1619

---
+++ sample/js/rensyu01.js
@@ -0,0 +1,25 @@
+
+//ボタンの設定
+$('.button').on('click',function(){
+    console.log($(this).attr('id'));
+    $("#container").removeClass();
+    $("#container").addClass($(this).attr('id'));
+  })
+  
+  //以下パーティクル風
+  let count = 12;//総数
+  let angle = 50;//角度の制限
+  
+  //繰り返し
+  for(let i=0;i<count;i++){
+    let rand= Math.random()*angle - angle/2;//ランダムな角度(angleで制限)
+    let obj=$('<div class="item"><div class="object"></div></div>');
+    $("#container").append(obj);
+   
+    setTimeout(function(){
+      obj.children('.object').addClass('anim');
+      obj.css({'transform':'rotate('+rand+'deg)'});
+    },i*100)
+  }
+  
+  
